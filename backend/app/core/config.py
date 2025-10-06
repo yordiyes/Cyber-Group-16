5824a3890e377e80e247a5975b598b17ac8a648e
Yordanos Yeshitla <yordiman614@gmail.com>
2025-10-06 22:39:59 +0300

    Set default value for SECRET_KEY in Settings class

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    SECRET_KEY: str
+    SECRET_KEY: str = "iMf1AVhxCwwps8WU"
 
     class Config:
         env_file = ".env"
